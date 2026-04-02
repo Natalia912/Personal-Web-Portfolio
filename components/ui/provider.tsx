b11741bd78136018542c375464de7c15ee3c88f4
Natalia Tretiakova <tretiakowa2212@gmail.com>
2026-04-02 12:23:33 +0500

    #1: remove tailwind, add and setup chakra ui

---
+++ components/ui/provider.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+
+import { PropsWithChildren } from "react";
+
+export function Provider({ children }: PropsWithChildren) {
+  return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>;
+}
